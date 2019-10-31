f912bf7d776c913c0d846f203ade0513d36eb152
Angela Vanessa <hansavanessa@gmail.com>
2019-10-31 20:48:56 +0700

    front end

--- scripts/main.js
+++ scripts/main.js
@@ -13,13 +13,13 @@ function topCharts(){
                 $(".top-charts").append(`
                     <div class="section">
                         <div class="row">
+                            <div class="col s1">${data[music].position}</div>
                             <div class="col s2"><img src="${data[music].album.cover_small}"></div>
-                            <div class="col s6">${data[music].title}</div>
-                            <div class="col s2">${data[music].artist.name}</div>
+                            <div class="col s5">${data[music].title}</div>
+                            <div class="col s4">${data[music].artist.name}</div>
                         </div>
                         <div class="divider"></div>
                     </div>
-                  
                 `);
             }
         })
@@ -33,15 +33,10 @@ function topArtists(){
         method: 'GET'
     })
         .done( ({data}) => {
-            for (let artist in data) {
-
-            $(".top-artists").append(`
-                <div class="col s3">
-                    <img src="${data[artist].picture_medium}">
-                </div>
-            `);
-
-                console.log(data[artist])
+            for (let i = 0; i < data.length - 1; i++) {
+                $(".top-artists").append(`
+                    <div class="col s4"><img src="${data[i].picture_medium}"></div>
+                `);
             }
         })
         .fail( err => {
