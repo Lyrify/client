26afa5c8e6622e3c3282229f932d9f2b9fdf506f
Angela Vanessa <hansavanessa@gmail.com>
2019-11-01 11:43:11 +0700

    almost done

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 $( document ).ready(function() {
+    $('#modal1').modal();
     topCharts();
     topArtists();
-    getLyric();
+    // topAlbums();
     console.log( "ready!" );
 });
 
@@ -13,7 +14,7 @@ function topCharts(){
             for (let music in data) {
                 $(".top-charts").append(`
                     <div class="section">
-                        <div class="row">
+                        <div class="row" onclick="getLyric('${data[music].artist.name}', '${data[music].title}')">
                             <div class="col s1">${data[music].position}</div>
                             <div class="col s2"><img src="${data[music].album.cover_small}"></div>
                             <div class="col s5">${data[music].title}</div>
@@ -36,29 +37,78 @@ function topArtists(){
         .done( ({data}) => {
             for (let i = 0; i < data.length - 1; i++) {
                 $(".top-artists").append(`
-                    <div class="col s4">
+                    <div class="col s4" onclick="showModal()">
                         <img src="${data[i].picture_medium}">
                         <p>${data[i].name}</p>
                     </div>
                 `);
+
+                $(".modal-content").empty()
+                $(".modal-content").append(`
+                    <h3>${data[i].name}</h3>
+                    <img src="${data[i].picture_medium}">
+                `);
             }
+            
         })
         .fail( err => {
             console.log(err);
         })
 }
 
-function getLyric(){
+function showModal() {
+    $('#modal1').modal('open');
+}
+
+function showHomepage() {
+    $(".homepage").show();
+    $(".lyric-page").hide();
+}
+
+// function topAlbums(){
+//     $.ajax('http://localhost:3000/music/top-albums', {
+//         method: 'GET'
+//     })
+//         .done( ({data}) => {
+//             for (let album in data) {
+               
+//                 // console.log(data[album].title)
+//             }
+//             // console.log(data)
+//         })
+//         .fail( err => {
+//             console.log(err);
+//         })
+// }
+
+function getLyric(artist, track){
     $.ajax({
         url: 'http://localhost:3000/music/lyric',
         method: 'GET',
-        params : {
-            artist : "Selena Gomez",
-            track: "Lose You To Love Me"
+        data : {
+            artist,
+            track
         }
     })
-    .done(({data}) => {
-        console.log('masuk sini')
+    .done( data => {
+        let lyrics = data.lyrics_body;
+        lyrics = lyrics.replace(/(?:\r\n|\r|\n)/g, '<br />');
+        $(".homepage").hide();
+        $(".lyric-section").empty();
+        $(".translation-section").empty();
+        $(".lyric-section").append(`
+            <div>
+                ${lyrics}
+            </div>
+        `);
+        $(".translation-section ").append(`
+            <div>
+                translation lyrics here
+            </div>
+        `);
+
+        $(".lyric-page").show();
+        
         console.log(data)
     })
     .fail(err => {
